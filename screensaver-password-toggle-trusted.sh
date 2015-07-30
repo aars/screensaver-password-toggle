0317b1200dab9b9652d758a2c89c3b8ce6283aa5
Aaron Heesakkers <aaronheesakkers@gmail.com>
2015-07-30 13:26:51 +0200

    Clean up.

--- screensaver-password-toggle-trusted.sh
+++ screensaver-password-toggle-trusted.sh
@@ -1,11 +1,23 @@
 #!/bin/sh
-TRUSTED_SSIDS="./.ssid.trusted"
-ENABLE="./screensaver-password-enable.applescript"
-DISABLE="./screensaver-password-disable.applescript"
-LOCK="./start-screensaver.applescript"
+
+# Where to find our list of trusted SSIDs
+if [ ! $TRUSTED_SSIDS ]; then
+	TRUSTED_SSIDS="./.ssid.trusted"
+fi
+
+if [ ! -f $TRUSTED_SSIDS ]; then
+	echo "Failed to load trusted SSIDs file $TRUSTED_SSIDS"
+	exit;
+fi
+
+# Scripts
+SCRIPT_ENABLE="./applescripts/screensaver-password-enable.applescript"
+SCRIPT_DISABLE="./applescripts/screensaver-password-disable.applescript"
+SCRIPT_STATE="./applescripts/screensaver-password-state.applescript"
+SCRIPT_LOCK="./applescripts/start-screensaver.applescript"
 
 function enable {
-	osascript $ENABLE
+	osascript $SCRIPT_ENABLE
 	if [ "$1" == "BSSID mismatch" ]
 	then
 		echo "Enabled: BSSID mismatch for SSID $SSID; Expected $TRUSTED_SSID_BSSID; Found $BSSID;"
@@ -15,11 +27,11 @@ function enable {
 
 	if [ ! "$STATE" == "true" ]; then
 		echo "Starting screensaver"
-		osascript $LOCK
+		osascript $SCRIPT_LOCK
 	fi
 }
 function disable {
-	osascript $DISABLE
+	osascript $SCRIPT_DISABLE
 	echo "Disabled: Connected to trusted SSID $SSID"
 }
 
@@ -27,7 +39,7 @@ function disable {
 cd "$(dirname "$0")"
 
 # Get current locking state.
-STATE=$(osascript ./screensaver-password-state.applescript)
+STATE=$(osascript $SCRIPT_STATE)
 
 # Get current SSID and BSSID
 SSID=$(./ssid.sh)
